396e68ab0423e286c524d93dbfc1835624e7d0ed
Carlos Valdemar Soria <carlosvsoria1982@gmail.com>
2022-07-20 19:09:57 -0300

    solve WARNINGS:?: (staticfiles.W004) The directory '/home/valdemar/Desktop/Publi/Carrusel/static' in the STATICFILES_DIRS setting does not exist

--- Carrusel/Carrusel/settings.py
+++ Carrusel/Carrusel/settings.py
@@ -132,6 +132,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'business/static'),
 )
